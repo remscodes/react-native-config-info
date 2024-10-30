99b17f91b15a0aac818df3d610b8b3caf8f5f5e9
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-10-30 19:41:45 +0100

    Update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,28 +6,28 @@ interface ConfigInfo {
   /**
    * Asynchronously returns the value associated to the provided key from Info.plist (iOS) or from resources strings.xml (Android).
    * @param key - The config key.
-   * @returns `null` if empty or missing
+   * @returns `null` if empty or missing.
    */
   get<T extends string = string>(key: string): Promise<T | null>;
 
   /**
    * Asynchronously returns the value associated to the provided key from Info.plist (iOS) or from resources strings.xml (Android).
    * @param key - The config key.
-   * @throws - if value is empty.
+   * @throws - if value is empty or missing.
    */
   getOrThrow<T extends string = string>(key: string): Promise<T>;
 
   /**
    * Synchronously returns the value associated to the provided key from Info.plist (iOS) or from resources strings.xml (Android).
    * @param key - The config key.
-   * @returns `null` if empty or missing
+   * @returns `null` if empty or missing.
    */
   getSync<T extends string = string>(key: string): T | null;
 
   /**
    * Synchronously returns the value associated to the provided key from Info.plist (iOS) or from resources strings.xml (Android).
    * @param key - The config key.
-   * @throws - if value is empty.
+   * @throws - if value is empty or missing.
    */
   getSyncOrThrow<T extends string = string>(key: string): T;
 
